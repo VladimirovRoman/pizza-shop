742589940426ceb079f6701a0962a5439325c083
VladimirovRoman <vladimirovrm@gmail.com>
2022-11-17 17:06:24 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
+import React from 'react'
 import './scss/app.scss'
 import Header from './components/Header'
 import Categories from './components/Categories'
 import Sort from './components/Sort'
 import PizzaBlock from './components/PizzaBlock'
-import pizzas from './assets/pizzas.json'
-
-//  https://63761837b5f0e1eb850277d5.mockapi.io/pizzas
 
 function App() {
-	fetch('https://63761837b5f0e1eb850277d5.mockapi.io/pizzas')
-		.then((response) => {
-			return response.json()
-		})
-		.then((json) => {
-			console.log(json)
-		})
+	const [items, setItems] = React.useState([])
+
+	React.useEffect(() => {
+		fetch('https://63761837b5f0e1eb850277d5.mockapi.io/pizzas')
+			.then((response) => response.json())
+			.then((json) => {
+				setItems(json)
+			})
+	}, [])
 
 	return (
 		<div className='wrapper'>
@@ -27,7 +27,7 @@ function App() {
 					</div>
 					<h2 className='content__title'>Все пиццы</h2>
 					<div className='content__items'>
-						{pizzas.map((obj) => (
+						{items.map((obj) => (
 							<PizzaBlock key={obj.id} {...obj} />
 						))}
 					</div>
