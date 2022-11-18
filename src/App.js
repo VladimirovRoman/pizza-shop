b49f3474cc97d4bcb4cbc5be183d0e32fbeeb144
VladimirovRoman <vladimirovrm@gmail.com>
2022-11-18 16:03:18 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,14 +13,14 @@ function App() {
 		<div className='wrapper'>
 			<Header />
 			<div className='content'>
-				<div className='container'>
+				
 					<Routes>
 						<Route path='/' element={<Home />} />
 						<Route path='/cart' element={<Cart />} />
 						<Route path='*' element={<NotFound />} />
 					</Routes>
 				</div>
-			</div>
+
 		</div>
 	)
 }
