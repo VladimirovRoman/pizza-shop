ded4d7b391ad55b653d4e96fc8bbe5a97861564e
VladimirovRoman <vladimirovrm@gmail.com>
2022-11-18 14:17:17 +0300

    Create NotFound.jsx

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NotFoundInfo from '../components/NotFoundInfo'
+
+
+const NotFound = () => {
+	return (
+		<h1>
+			<NotFoundInfo />
+		</h1>
+	)
+}
+
+export default NotFound
