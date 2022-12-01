71659c4c8ce1d251834aca780a1b12d736809878
VladimirovRoman <vladimirovrm@gmail.com>
2022-12-01 14:45:42 +0300

    deploy

--- src/pages/FullPizza.tsx
+++ src/pages/FullPizza.tsx
@@ -1,43 +1,48 @@
-import React from 'react'
-import axios from 'axios'
-import { useParams, useNavigate } from 'react-router-dom'
-import { stringify } from 'querystring'
+import React from 'react';
+import axios from 'axios';
+import { useParams, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const FullPizza: React.FC = () => {
 	const [pizza, setPizza] = React.useState<{
-		imageUrl: string
-		price: number
-		title: string
-	}>()
+		imageUrl: string;
+		price: number;
+		title: string;
+	}>();
 
-	const { id } = useParams()
-	const navigate = useNavigate()
+	const { id } = useParams();
+	const navigate = useNavigate();
 
 	React.useEffect(() => {
 		async function fetchPizza() {
 			try {
 				const { data } = await axios.get(
 					'https://63761837b5f0e1eb850277d5.mockapi.io/pizzas/' + id
-				)
-				setPizza(data)
+				);
+				setPizza(data);
 			} catch (error) {
-				alert('Ошибка при получении данных')
-				navigate('/')
+				alert('Ошибка при получении данных');
+				navigate('/');
 			}
 		}
-		fetchPizza()
-	}, [])
+		fetchPizza();
+	}, []);
 
 	if (pizza === undefined) {
-		return <> Загрузка ...</>
+		return <> Загрузка ...</>;
 	}
 	return (
 		<div className='container'>
 			<img src={pizza.imageUrl} alt='' />
 			<h2>{pizza.title}</h2>
 			<h4>{pizza.price} </h4>
+			<Link to='/'>
+				<button className='button button--outline button--add'>
+					<span>Назад</span>
+				</button>
+			</Link>
 		</div>
-	)
-}
+	);
+};
 
-export default FullPizza
+export default FullPizza;
